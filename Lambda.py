c196d4c2cd867d9e75aba95ee31bbf161b16be1d
qwertyuiop12as <78254430+qwertyuiop12as@users.noreply.github.com>
2021-03-13 15:02:47 +0900

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -1,1 +1,12 @@
+#Original function
+def square_original(num) :
+  result = num ** 2
 
+value = square_original(4)
+print(value)
+
+#Using Lambda
+square = lambda num: num ** 2
+
+value_lambda = square(2)
+print(value_lambda)
